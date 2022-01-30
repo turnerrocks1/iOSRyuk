361532df6012a5cf3df4dea89218df9e9e69d802
Turnerhackz1 <turner881189@gmail.com>
2022-01-30 16:09:07 -0600

    Update iOSRyuk.py

--- iOSRyuk.py
+++ iOSRyuk.py
@@ -329,6 +329,114 @@ def parseGOTNames():
                     SetType(currentEA, "struct vtable_" + className + "*")
                 currentEA += 0x8
 
+                
+def registersysctlstructs():
+    '''
+    registersysctlstructs:
+    
+    Registers the 'sysctl_req' and 'sysctl_oid' struct types in IDA.
+    '''
+#oldptr is really user_addr_t but ida pro doesn't have it natively
+
+fixupsysctl_req = """
+
+struct sysctl_req {
+    struct proc     *p;
+    int             lock;
+    unsigned int     oldptr;         
+    size_t          oldlen;         
+    size_t          oldidx;         
+    int             (*oldfunc)(struct sysctl_req *, const void *, size_t);
+    unsigned int     newptr;         
+    size_t          newlen;         
+    size_t          newidx;         
+    int             (*newfunc)(struct sysctl_req *, void *, size_t);
+};
+
+"""
+
+fixupsysctl_oid = """ 
+
+struct sysctl_oid {
+    void *oid_parent;
+    void *oid_link;
+    int             oid_number;
+    int             oid_kind;
+    void            *oid_arg1;
+    int             oid_arg2;
+    const char      *oid_name;
+    int             (*oid_handler) (struct sysctl_oid *oidp, void *arg1, int arg2, struct sysctl_req *req);
+    const char      *oid_fmt;
+    const char      *oid_descr; 
+    int             oid_version;
+    int             oid_refcnt;
+};
+
+"""
+
+set_local_type(-1, fixupsysctl_req, 0)
+import_type(-1, "sysctl_req")
+set_local_type(-1, fixupsysctl_oid, 0)
+import_type(-1, "sysctl_oid")
+
+#https://opensource.apple.com/source/xnu/xnu-7195.81.3/bsd/sys/sysctl.h.auto.html
+def fixupSysctlSet():
+    '''
+    fixupSysctlSet:
+    
+    Fixes up the '__sysctl_set' segment, ensures the targets are actually
+    'sysctl_oid' structures and adds the correct function type to the handler.
+    '''
+    
+    segm = idaapi.get_segm_by_name("__DATA_CONST.__sysctl_set")
+    if not segm:
+        print ("Could not find __sysctl_set segment")
+        return
+        
+    start = segm.start_ea
+    end = segm.end_ea
+
+    sid = get_struc_id("sysctl_oid") #might be sysctl_oid_list
+    ssize = get_struc_size(sid)
+    stru = get_struc(sid)
+    if ssize == 0:
+        print ("Could not load information about 'sysctl_oid' struct")
+        return
+
+    # clear whole range of sysctl_set segment
+    #idaapi.do_unknown_range(start, end-start, DOUNK_DELNAMES) 
+    #this is outdated
+    ida_bytes.del_items(start, DELIT_DELNAMES, end-start)
+
+    # idapython oldschool - we work with the structure offset
+    oid_handler = get_member_by_name(stru, "oid_handler")
+    
+    # loop through sysctl_set segment
+    while start < end:
+        # Ensure pointer is a pointer
+        #idaapi.op_offset(start, 0, idaapi.REF_OFF32, 0xffffffff, 0, 0)
+        idaapi.op_offset(start, 0, idaapi.REF_OFF32, 0xffffffff, 0, 0)
+        ptr = get_qword(start)
+
+        print("debug func struct ptr:" + str(hex(ptr)));
+        
+        # Mark structure as sysctl_oid structure
+        #idaapi.do_unknown_range(ptr, ssize, DOUNK_DELNAMES)
+        #outdated
+        ida_bytes.del_items(ptr, DELIT_DELNAMES, ssize)
+        #x = doStruct(ptr, ssize, sid)
+        x = ida_bytes.create_struct(ptr, ssize, sid)
+        handler = get_qword(ptr + oid_handler.soff)
+
+        # We have to support ARM THUMB code
+        #addr = handler & 0xFFFFFFFE
+        addr = handler & 0xfff
+        print("debug func struct addr:" + str(hex(handler)));
+        # Set correct function type for oid_handler
+        idc.SetType(addr, "int *oid_handler(struct sysctl_oid *oidp, void *arg1, int arg2, struct sysctl_req *req);")
+
+        start += 8
+            
 def main():
     parseGOTNames()
     preparePredefinedStructNameToIdMap()
@@ -339,6 +447,10 @@ def main():
     print ("[+] Process All Function Args")
     processAllFuncArgs()
     hideAllStructs()
+    print("[+] Fixing up sysctl_set")
+    registersysctlstructs()
+    fixupSysctlSet()
+    print("[-] Finished")
 
 if __name__ == "__main__":
     main()
